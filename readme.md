f5d39b7e7bd1742261703d9ce5eb3664dfd39cce
vladimir.vlasenko <v.g.vlasenko@yandex.ru>
2023-09-13 21:55:14 +0300

    FIX: screenshot in readme file + disable findAll() in ContactInformationRepository

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-Swagger docs at http://localhost:8080/swagger-ui/index.html#/ after run application
+Swagger at http://localhost:8080/swagger-ui/index.html#/ after run application
 
-![img_1.png](img_1.png)
+![img.png](img.png)
--- src/main/java/ru/unibell/vladimirvlasenkoskilltest/repository/ContactInformationRepository.java
+++ src/main/java/ru/unibell/vladimirvlasenkoskilltest/repository/ContactInformationRepository.java
@@ -1,7 +1,13 @@
 package ru.unibell.vladimirvlasenkoskilltest.repository;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.unibell.vladimirvlasenkoskilltest.model.ContactInformation;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -13,4 +19,32 @@ public interface ContactInformationRepository extends GenericRepository<ContactI
     Set<String> findEmailsByClientId(Long id);
 
     Set<String> findPhonesByClientId(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    List<ContactInformation> findAll();
+
+    @Override
+    @RestResource(exported = false)
+    List<ContactInformation> findAllById(Iterable<Long> longs);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ContactInformation> List<S> findAll(Example<S> example);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ContactInformation> List<S> findAll(Example<S> example, Sort sort);
+
+    @Override
+    @RestResource(exported = false)
+    List<ContactInformation> findAll(Sort sort);
+
+    @Override
+    @RestResource(exported = false)
+    Page<ContactInformation> findAll(Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ContactInformation> Page<S> findAll(Example<S> example, Pageable pageable);
 }
